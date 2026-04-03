46f8416541589493332e2a2dffcb9712285129f6
Agustin <agustinescuderoweb@gmail.com>
2026-04-02 23:09:37 -0300

    nuevo tamaño fonts

--- lib/generarImagen.ts
+++ lib/generarImagen.ts
@@ -22,7 +22,7 @@ export async function generarImagen(numero: string) {
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
 
-  ctx.fillText(numero, image.width / 2, image.height * 0.65);
+  ctx.fillText(numero, image.width / 2, image.height * 0.655);
 
   return canvas.toBuffer("image/jpeg");
 }
